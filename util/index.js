93a08433e23cffaadc8e14079a2a41b575101773
James Crosslin <jamesmscrosslin@gmail.com>
2021-06-02 15:58:33 -0700

    handle sql validation errors

--- util/index.js
+++ util/index.js
@@ -4,7 +4,17 @@ module.exports = {
       try {
         return await cb(req, res, next);
       } catch (err) {
-        if (!res.headersSent) return next(err);
+        if (res.headersSent) return console.error('Error occurred after header sent: ', err);
+
+        if (
+          err.name === 'SequelizeValidationError' ||
+          err.name === 'SequelizeUniqueConstraintError'
+        ) {
+          err.validationErrors = err.errors.map((err) => err.message);
+          err.status = 400;
+        }
+
+        return next(err);
       }
     };
   },
