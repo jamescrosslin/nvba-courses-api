e210fdd62c9ebb7370324c495e69b4f23869be8b
James Crosslin <jamesmscrosslin@gmail.com>
2021-06-03 11:19:24 -0700

    replaced user.js with users.js

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const { User } = require('../models');
+const { authenticateUser, asyncHandler } = require('../util');
 
 router
   .route('/')
-  .get(async (req, res) => {
-    try {
+  .get(
+    authenticateUser,
+    asyncHandler(async (req, res) => {
       const users = await User.findAll();
       res.json(users);
-    } catch (err) {
-      console.warn(err);
-    }
-  })
-  .post(async (req, res) => {
-    const { firstName, lastName, emailAddress, password } = req.body;
-    try {
+    }),
+  )
+  .post(
+    asyncHandler(async (req, res) => {
+      const { firstName, lastName, emailAddress, password } = req.body;
       await User.create({ firstName, lastName, emailAddress, password });
       res.status(201).json({ message: 'User Created' });
-    } catch (err) {
-      console.error(err);
-    }
-  });
+    }),
+  );
 
 module.exports = router;
