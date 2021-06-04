aa596370ebcc8ff1c4432e85aef133b03fd2f2bf
James Crosslin <jamesmscrosslin@gmail.com>
2021-06-03 19:47:24 -0700

    added error message for invalid request

--- util/index.js
+++ util/index.js
@@ -8,14 +8,13 @@ module.exports = {
       try {
         return await cb(req, res, next);
       } catch (err) {
-        if (res.headersSent) return console.error('Error occurred after header sent: ', err);
-
         if (
           err.name === 'SequelizeValidationError' ||
           err.name === 'SequelizeUniqueConstraintError'
         ) {
           err.validationErrors = err.errors.map((err) => err.message);
           err.status = 400;
+          err.message = 'Submission was invalid.';
         }
 
         return next(err);
